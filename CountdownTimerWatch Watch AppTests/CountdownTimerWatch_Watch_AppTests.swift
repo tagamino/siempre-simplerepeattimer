4b45b62b552be7c5baae8c4e5a7c1652c7b07e88
tagamino <dontagamino@gmail.com>
2025-10-12 18:02:04 +0900

    SiempreのgoogleAD設定

---
+++ CountdownTimerWatch Watch AppTests/CountdownTimerWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerWatch_Watch_AppTests.swift
+//  CountdownTimerWatch Watch AppTests
+//
+//  Created by tagamino on 2025/10/11.
+//
+
+import Testing
+@testable import CountdownTimerWatch_Watch_App
+
+struct CountdownTimerWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ CountdownTimerWatch Watch AppUITests/CountdownTimerWatch_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CountdownTimerWatch_Watch_AppUITestsLaunchTests.swift
+//  CountdownTimerWatch Watch AppUITests
+//
+//  Created by tagamino on 2025/10/11.
+//
+
+import XCTest
+
+final class CountdownTimerWatch_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
